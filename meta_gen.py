86d093356a626bea29eae08578fad48e1ee09088
Jonathan Ray <Jonathanallenray1@gmail.com>
2018-09-06 22:28:22 -0500

    Added Collatz Meta Cache Generator

---
+++ meta_gen.py
@@ -0,0 +1,13 @@
+
+from Collatz import collatz_eval
+from sys     import stdin, stdout
+
+if __name__ == "__main__" :
+    stdout.write('[')
+    for n in range(1, 1000):
+        num = collatz_eval (0 + n*1000, 1000 + 1000*n)
+        stdout.write(str(num) + ',')
+        if n % 10 == 0:
+            stdout.write(str(num) + '\n')
+    stdout.write(']')
+
